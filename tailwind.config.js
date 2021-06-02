5b30a390bd71467b5d120662e7cbe9113f371891
asmae rafie <asmae.rafie21@gmail.com>
2021-06-02 23:29:51 +0100

    tailwind version

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    minWidth: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      'full': '100%',
+     }, 
+     boxShadow: {
+      sm: '0 0px 10px 8px rgba(0,0,0,0.19)',
+    },
+    // width: {
+    //   "90": "90vw",
+    // },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
